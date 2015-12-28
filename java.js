8be8747f034d694cf3ff4183e493b6f9c05ff8f4
Ago Ugo <clintwine@gmail.com>
2015-12-28 21:46:28 +0000

    intial java.js file

---
+++ java.js
@@ -0,0 +1,70 @@
+var help = true;
+if (help === true) {
+    console.log("yes it is true");
+    
+}else {
+    
+}
+
+
+
+
+
+
+
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person ("George Washington" , 275);
+
+/**********************************************/
+
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+// make a Dog constructor here
+function Dog(name , breed) {
+    this.name = name;
+    this.bredd = breed;
+}
+
+/**********************************************/
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.species = "Homo Sapiens";
+}
+
+var sally = new Person("Sally Bowles", 39);
+var holden = new Person("Holden Caulfield" , 16);
+console.log("sally's species is " + sally.species + " and she is " + sally.age);
+console.log("holden's species is " + holden.species + " and he is " +holden.age );
+
+
+/**********************************************/
+
+function Rectangle(height, width) {
+  this.height = height;
+  this.width = width;
+  this.calcArea = function() {
+      return this.height * this.width;
+  };
+  // put our perimeter function here!
+  this.calcPerimeter = function(){
+      return(this.height + this.width )*2;
+  };
+}
+
+var rex = new Rectangle(7,3);
+var area = rex.calcArea();
+var perimeter = rex.calcPerimeter();
